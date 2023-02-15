bac19c91ed4fd219e13aa9fa0f28ea12f74f12b4
yummygoods <melissa@yummygoods.com>
2023-02-15 15:46:25 -0500

    got the db to populate from form

--- src/main/resources/static/poopReportFrontEnd/app.js
+++ src/main/resources/static/poopReportFrontEnd/app.js
@@ -2,14 +2,20 @@ console.log("oh hi this should show up");
 /*++++++++++++++++++++++++++++++++++++++++++++++++
  function to reset after submit
 ++++++++++++++++++++++++++++++++++++++++++++++++*/
+
 function reset() {
   //.................................tells the dom to remove the user's text input
-  document.getElementById('namedog').value = '';
+  document.getElementById('dog_id').value = '';
+  document.getElementById('poop').checked = '';
+   document.getElementById('pee').checked = '';
   document.getElementById('notes').value = '';
   //.................................resets the values stored in the variables
-  name = '';
+  dog_id = '';
+  poop ='';
+  pee = '';
   notes = '';
 };
+
 
 ///////////////////// end of function ////////////////////
 
@@ -19,12 +25,12 @@ function reset() {
 let poopForm = document.getElementById('poopForm');
 poopForm.addEventListener('submit', function (event) {
   event.preventDefault();
-/*  addFromForm();
-  reset();*/
+  addFromForm();
+  reset();
 /*debugger;*/
+console.log(event.target.elements.dog_id.value);
 console.log(event.target.elements.poop.checked);
 console.log(event.target.elements.pee.checked);
-console.log(event.target.elements.name.value);
 console.log(event.target.elements.notes.value);
 });
 
@@ -35,13 +41,15 @@ console.log("now listening for submit button");
 class to capture form input in an object
 ++++++++++++++++++++++++++++++++++++++++++++++++*/
 class DogEvent {
-  constructor(name, notes) {
-    this.name = name;
+  constructor(dog_id, poop, pee, notes) {
+    this.dog_id = dog_id;
+    this.poop = poop;
+    this.pee = pee;
     this.notes = notes;
   }
 }
 
-console.log("this should print the newly created class Track: ", Track);
+console.log("this should print the newly created class DogEvent: ", DogEvent);
 
 
 
@@ -51,24 +59,26 @@ function to take form input and turn into object
 ++++++++++++++++++++++++++++++++++++++++++++++++*/
 function addFromForm() {
   // .................................takes input values and saves as variables name + notes
-  let name = document.getElementById('name').value;
-  console.log("this should print the value from user input in Name field:", name);
+  let dog_id = document.getElementById('dog_id').value;
+  console.log("this should print the value from user input in dog name (dog_id)  field:", dog_id);
+/*debugger;*/
 
  let poop = document.getElementById('poop').checked;
   console.log("this should print the the boolean value from user input in poop field: ", poop);
 
- let poop = document.getElementById('pee').checked;
+  let pee = document.getElementById('pee').checked;
   console.log("this should print the the boolean value from user input in pee field: ", pee);
 
   let notes = document.getElementById('notes').value;
   console.log("this should print the value from user input in Notes field: ", notes);
 
   // .................................instantiates new Track object and passes variables defined above as arguments into the constructor
-  const trackFromForm = new Track(name, notes);
-  console.log("this should print the new track object with values:", trackFromForm);
+  const dogEventFromForm = new DogEvent(dog_id, poop, pee, notes);
+  console.log("this should print the new dogEvent object with values:", dogEventFromForm);
 
   // .................................calls function to take the new track object and make a post request with it
-  sendTrackToServer(trackFromForm);
+  sendDogEventToServer(dogEventFromForm);
+
 
 
 }
@@ -78,15 +88,17 @@ function addFromForm() {
 
 
 /*++++++++++++++++++++++++++++++++++++++++++++++++
-function to send the new track object to the db
+function to send the new dogEvent object to the db
 ++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-function sendTrackToServer(Track) {
+/*function sendDogEventToServer(DogEvent) {*/
+function sendDogEventToServer(dogEventFromForm) {
   fetch(
 
-    'http://localhost:8080/tracks',
+    'http://localhost:8080/events',
     {
-      method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(Track)
+     /* method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(DogEvent)*/
+        method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(dogEventFromForm)
     }
   ).then(response => {
 
@@ -99,5 +111,5 @@ function sendTrackToServer(Track) {
   })
 }; 
 
- //.................................turns the new track object into json and sends as request body
+ //.................................turns the new dogEvent object into json and sends as request body
 ///////////////////// end of function /////////////////////
