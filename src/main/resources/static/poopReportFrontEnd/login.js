6946604f9594459b7bc6d7eb67dc9c4acfc40f7d
yummygoods <melissa@yummygoods.com>
2023-02-27 16:26:26 -0500

    add form to create profile

--- src/main/resources/static/poopReportFrontEnd/login.js
+++ src/main/resources/static/poopReportFrontEnd/login.js
@@ -1,1 +1,9 @@
-let login = document.getElementById(login)
+let login = document.getElementById("login");
+login.addEventListener('submit', (event)=> {
+event.preventDefault();
+
+const user_name = document.getElementById('user_name').value;
+const password = document.getElementById('password').value;
+
+if
+})
