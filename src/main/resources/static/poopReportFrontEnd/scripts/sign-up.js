feca51dc60624804f3b74c06ad6bec732c83df13
yummygoods <melissa@yummygoods.com>
2023-04-04 15:30:13 -0400

    WIP add responsiveness and success message on signup

--- src/main/resources/static/poopReportFrontEnd/scripts/sign-up.js
+++ src/main/resources/static/poopReportFrontEnd/scripts/sign-up.js
@@ -1,6 +1,5 @@
 
 function sendData() {
-
     // Create and populate the FormData to send
     theForm = document.getElementById("createUser");
     data = new FormData(theForm);
@@ -16,9 +15,13 @@ let jsonData = JSON.stringify(jsonObject);
             }
     }).then(response => {
         return response.json();
-/*    }).then(text => {
-        console.log(text);*/
-    }).catch(err => {
+
+    }).then(response => {
+    redirectToProfilePage();
+    showSuccessMessage();
+
+    })
+.catch(err => {
         console.log(err);
     });
 }
@@ -27,4 +30,25 @@ let jsonData = JSON.stringify(jsonObject);
         sendData();
         e.preventDefault();
      theForm.reset();
+       console.log("just reset the form" );
+
     });
+
+ /* function showSuccessMessage(){
+    const successMessage = alert("yay! ok now it's time to make your profile" );
+      console.log("inside the function to show the success message");
+    }*/
+
+function redirectToProfilePage() {
+  // redirect to profile page after 3 seconds
+  setTimeout(() => {
+    window.location.href = 'http://localhost:63342/poopReport/static/poopReportFrontEnd/profile-page.html';
+  }, 1000);
+}
+//show success message by inserting html
+  function showSuccessMessage(){
+    const successMessage = document.createElement('div');
+    successMessage.textContent = "yay etc now make your profile below"
+    document.body.appendChild(successMessage);
+      console.log("inside the function to show the success message");
+    }
