233483645a314244102f10ea52c671e0016a3e84
yummygoods <melissa@yummygoods.com>
2023-04-15 12:18:10 -0400

    added BasicAuthProperties file

--- src/main/java/config/SecurityConfiguration.java
+++ src/main/java/config/SecurityConfiguration.java
@@ -1,5 +1,10 @@
 package config;
 
+//USED THIS REFERENCE https://reflectoring.io/spring-security/
+//and updated from ant matchers specified there to request matchers: https://docs.spring.io/spring-security/reference/5.8/migration/servlet/config.html
+
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableConfigurationProperties(BasicAuthProperties.class)
 public class SecurityConfiguration {
 
     public static final String[] ENDPOINTS_WHITELIST = {
@@ -27,21 +33,36 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                                                            .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                                                            .requestMatchers("/api/user/**").hasRole("USER")
+                                                            // .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                                                            // .requestMatchers("/api/user/**").hasRole("USER")
+                                                            // trying without "api" below
+                                                            .requestMatchers("/admin/**").hasRole("ADMIN")
+                                                            .requestMatchers("/user/**").hasRole("USER")
                                                             .anyRequest().authenticated()
                 )
-                .csrf().disable()
+                /* .csrf().disable()*/
                 .formLogin(form -> form
                                            .loginPage(LOGIN_URL)
                                            .loginProcessingUrl(LOGIN_URL)
                                            .failureUrl(LOGIN_FAIL_URL)
                                            .usernameParameter(USERNAME)
                                            .passwordParameter(PASSWORD)
-                                           .defaultSuccessUrl(DEFAULT_SUCCESS_URL)
-                );
+                                           .defaultSuccessUrl(DEFAULT_SUCCESS_URL))
+             /*   .logout(logout -> logout
+                                          .logoutUrl("/logout")
+                                          .invalidateHttpSession(true)
+                                          .deleteCookies("JSESSIONID")
+                                          .logoutSuccessUrl(LOGIN_URL + "?logout"))
+                .sessionManagement(session -> session
+                                                      .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                                                      .invalidSessionUrl("/invalidSession.htm")
+                                                      .maximumSessions(1)
+                                                      .maxSessionsPreventsLogin(true))*/
+
+        ;
 
 
         return http.build();
     }
+
 }
