01344683d6ea9dfaa126c68404d835954a01f96b
yummygoods <melissa@yummygoods.com>
2023-06-02 22:52:10 -0400

    add logo + change css

--- src/main/resources/static/poopReportFrontEnd/scripts/confetti.js
+++ src/main/resources/static/poopReportFrontEnd/scripts/confetti.js
@@ -1,20 +1,23 @@
- <script src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"
-            defer></script>
+/* <script src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"
+            defer></script>*/
 
 
-            import JSConfetti from 'js-confetti';
+       //     import JSConfetti from 'js-confetti';
 
 
 
             function confetti(){
             const jsConfetti = new JSConfetti();
-            /*jsConfetti.addConfetti();*/
-            jsConfetti.addConfetti({
-              emojis: ['🦄'],
-              emojiSize: 100,
-              confettiNumber: 30,
-            })
-              setTimeout(() => {
-             jsConfetti.clearCanvas, 4000);
+//            jsConfetti.addConfetti();
+//            jsConfetti.addConfetti({
+//              emojis: ['🦄'],
+//              emojiSize: 100,
+//              confettiNumber: 30,
+//            })
+//              setTimeout(() => {
+//             jsConfetti.clearCanvas, 4000);
+//
+//            };
+//
 
-            };
+             jsConfetti.addConfetti();
